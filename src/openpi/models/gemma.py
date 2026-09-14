Fix broadcast parameter in gemma.py
上次提交中误修改了nn.scan的类型，导致测试出现报错，debug

src/openpi/models/gemma.py
@@ -471,7 +471,7 @@ class Module(nn.Module):
                 nn.broadcast,
                 nn.broadcast,
                 nn.broadcast,
-                nn.broadcast,
+                0,
                 nn.broadcast,
             ),
             length=self.configs[0].depth,